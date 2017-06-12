4e8e87cde99d6f2b4b8b9bede9a13142207ac3fb
Kalyan Asaldhar <kalyan.asaldhar@gmail.com>
2017-06-12 13:44:13 -0400

    Using @Qualifier annotation to demonstrate the autowiring when more than one implementations present for the interface.

--- SpringDemoAnnotations/src/com/springtraining/CricketCoach.java
+++ SpringDemoAnnotations/src/com/springtraining/CricketCoach.java
@@ -1,12 +1,14 @@
 package com.springtraining;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
 	
 	@Autowired
+	@Qualifier("happyFortuneService")
 	public FortuneService fortuneService;
 
 	@Override
